b4373870f8cde009e3464e2c0ea20276eca11a27
Abhishek-Kartik <abhishek123cu@gmail.com>
2024-02-25 19:03:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CurrentLocation from './currentLocation'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <CurrentLocation/>
+      </div>
+
+      <div className="footer-info">
+       Developed by{" "}Abhishek Kumar
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
